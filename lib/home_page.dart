7072b07add10ebdd8554e8dea37dbcf5d3971bb1
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-30 02:11:46 +0700

    membuat struktur dasar halaman HomePage dengan AppBar dan body

---
+++ lib/home_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1flutter/data_piket.dart';
+import 'package:ucp1flutter/data_pelanggan.dart';
+import 'package:ucp1flutter/barang_masuk_keluar.dart';
+import 'package:ucp1flutter/login_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.greenAccent,
+        automaticallyImplyLeading: false,
+        title: Column(
+          children: [
+           
+          ],
+        ),
+      ),
+
+      body: Padding(
+        key: _formKey,
+        padding: const EdgeInsets.only(top: 16.0),
+        child: Column(
+          children: [
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
