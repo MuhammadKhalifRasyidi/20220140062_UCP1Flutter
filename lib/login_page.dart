73eba3c77f8f420c2ae4079a92dfd3d16052bd48
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-29 23:47:55 +0700

    menambahkan struktur dasar halaman login

---
+++ lib/login_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1flutter/home_page.dart';
+import 'package:ucp1flutter/register_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool _obscurePassword = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+               
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
