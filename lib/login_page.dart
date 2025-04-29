312940b1c4d0f65c18537cea164f5ae587a5429e
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-29 23:59:17 +0700

    menambahkan tombol login dan navigasi ke halaman Home

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -81,6 +81,28 @@ class _LoginPageState extends State<LoginPage> {
                     return null;
                   },
                 ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.greenAccent,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 165,
+                      vertical: 16,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomePage()),
+                      );
+                    }
+                  },
+                  child: Text('Masuk'),
+                ),
                 
               ],
             ),
