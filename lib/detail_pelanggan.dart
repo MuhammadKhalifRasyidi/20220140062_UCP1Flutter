d7ec76a9f30b056bc472116f87adb6c109406d90
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-30 22:08:47 +0700

    menambahkan form input alamat, provinsi, dan kode pos serta validasinya

--- lib/detail_pelanggan.dart
+++ lib/detail_pelanggan.dart
@@ -35,6 +35,75 @@ class _DetailPelangganState extends State<DetailPelanggan> {
               children: [
                 Image.asset('assets/images/kambing.png', height: 120),
                 Text('SELAMAT DATANG KEMBALI'),
+                const SizedBox(height: 25),
+                Align(alignment: Alignment.centerLeft, child: Text('Alamat')),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: alamatController,
+                  decoration: const InputDecoration(
+                    labelText: 'Alamat',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Alamat tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 25),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Provinsi'),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: provinsiController,
+                            decoration: const InputDecoration(
+                              labelText: 'Provinsi',
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.email),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Provinsi tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Kode Pos'),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: kodePosController,
+                            decoration: const InputDecoration(
+                              labelText: 'Kode Pos',
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(Icons.phone),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Kode Pos tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
                 
               ],
             ),
