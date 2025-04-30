6ed3d0b245858af75583bd4b7706ff3aa26e4f07
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-30 21:53:46 +0700

    membuat struktur awal halaman DataPelanggan dengan AppBar dan Form

---
+++ lib/data_pelanggan.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1flutter/detail_pelanggan.dart';
+
+class DataPelanggan extends StatefulWidget {
+  const DataPelanggan({super.key});
+
+  @override
+  State<DataPelanggan> createState() => _DataPelangganState();
+}
+
+class _DataPelangganState extends State<DataPelanggan> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController namaCustController = TextEditingController();
+  final TextEditingController emailCustController = TextEditingController();
+  final TextEditingController noHpCustController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController kodePosController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Text(
+          'Data Pelanggan',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Align(alignment: Alignment.centerLeft, child: Text('Nama Cust')),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: namaCustController,
+                decoration: const InputDecoration(
+                  labelText: 'Nama Cust',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama Cust tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 25),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Email'),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          controller: emailCustController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Email tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('No HP'),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          controller: noHpCustController,
+                          decoration: const InputDecoration(
+                            labelText: 'No HP',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.phone),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'No HP tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 25),
+              Align(alignment: Alignment.centerLeft, child: Text('Alamat')),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: alamatController,
+                decoration: const InputDecoration(
+                  labelText: 'Alamat',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Alamat tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 25),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Provinsi'),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          controller: provinsiController,
+                          decoration: const InputDecoration(
+                            labelText: 'Provinsi',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Provinsi tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Kode Pos'),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          controller: kodePosController,
+                          decoration: const InputDecoration(
+                            labelText: 'Kode Pos',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.phone),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Kode Pos tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.greenAccent,
+                  minimumSize: Size(900, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  foregroundColor: Colors.black
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPelanggan(),
+                      ),
+                    );
+                  }
+                },
+                child: Text('Simpan'),
+              ),
+              SizedBox(height: 25),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(900, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(color: Colors.black, width: 1),
+                  ),
+                  foregroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  namaCustController.clear();
+                  emailCustController.clear();
+                  noHpCustController.clear();
+                  alamatController.clear();
+                  provinsiController.clear();
+                  kodePosController.clear();
+                },
+                child: Text('Reset'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
