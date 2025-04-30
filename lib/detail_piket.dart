0bac0102ab9a750e5e2ee7648de689f300b643a2
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2025-04-30 14:57:53 +0700

    buat halaman DetailPiket dengan parameter nama dan tugasList

---
+++ lib/detail_piket.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DetailPiket extends StatelessWidget {
+  final String nama;
+  final List<String> tugasList;
+
+  const DetailPiket({Key? key, required this.nama, required this.tugasList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
